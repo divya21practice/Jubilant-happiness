8bd4d94da13a5ffd8e106448d5b10f42545c20cf
divya20-practice <divyakeerthi14@gmail.com>
2021-02-11 17:22:14 -0500

    varable update

--- src/java/Student.java
+++ src/java/Student.java
@@ -1,2 +1,22 @@
 public class Student {
+
+    String address;
+    int grade;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
 }
+
